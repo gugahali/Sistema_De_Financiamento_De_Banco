6d65e2ddd0b332bfc179e3f7b29bf01867b6dfc2
gugahali <halickigustavo@gmail.com>
2025-05-22 13:42:12 -0300

    classes

--- Main.java
+++ Main.java
@@ -1,6 +1,31 @@
-public class Main {
-    public static void main(String[] args){
-     System.out.println("Ola, Aqui Começa o Codigo");   
+import java.util.Scanner;
 
+public class Main {
+    
+    //Atributos
+    double valorImovel;
+    int parcelas;
+    double jurosMensais;
+
+    //Metodos
+    Main(double ValorImovelUsuario, int ParcelasCasa){
+        this.valorImovel = ValorImovelUsuario;
+        this.parcelas = ParcelasCasa;
+        return;
+    }
+
+    double Valorparcelas(){
+            return valorImovel / parcelas;
+    }
+
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);  
+        System.out.println(("Qual o valor da casa?"));
+        double ValorCasaUsuario = sc.nextDouble();
+        System.out.println("Quantas parcelas?");
+        int ParcelasUsuario = sc.nextInt();
+        Main Casa1 = new Main(ValorCasaUsuario,ParcelasUsuario);
+        System.out.println("Valor Mensal das Parcelas:"+Casa1.Valorparcelas());
     }     
 }
